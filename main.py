fc56d05a5da2b0815a7ec42fb455c9f483e16211
Shreshth Tuli <shreshthtuli@gmail.com>
2021-06-22 10:27:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ def initalizeEnvironment(environment, logger):
 	
 	# Initialize recovery
 	''' Can be Recovery, DeepFTRecovery, PCFTRecovery, TopoMADRecovery, DFTMRecovery, ECLBRecovery, AWGGRecovery '''
-	recovery = TopoMADRecovery(HOSTS, environment)
+	recovery = AWGGRecovery(HOSTS, environment)
 
 	# Initialize Environment
 	hostlist = datacenter.generateHosts()
